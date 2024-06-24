abd48b465546fed6ce759fb9bc08c62fe7986ee4
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2024-06-24 12:52:06 +0200

    HW-2/react

---
+++ src/components/App/App.jsx
@@ -0,0 +1,55 @@
+import css from "./App.module.css";
+import { useState, useEffect } from "react";
+
+import Description from "../Description /Description";
+import Feedback from "../Feedback/Feedback";
+import Options from "../Options/Options";
+import Notification from "../Notification/Notification";
+
+export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = window.localStorage.getItem("feedback");
+    return savedFeedback ? JSON.parse(savedFeedback) : initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const initialState = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback =
+    Math.round((feedback.good / totalFeedback) * 100) || 0;
+
+  return (
+    <section className={css.container}>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={initialState}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </section>
+  );
+}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,18 @@
+import css from "./Feedback.module.css";
+export default function Feedback({
+  good,
+  neutral,
+  bad,
+  totalFeedback,
+  positiveFeedback,
+}) {
+  return (
+    <div>
+      <p className={css.text}>Good: {good} </p>
+      <p className={css.text}>Neutral: {neutral} </p>
+      <p className={css.text}>Bad: {bad}</p>
+      <p className={css.text}>Total: {totalFeedback}</p>
+      <p className={css.text}> Positive:{positiveFeedback}%</p>
+    </div>
+  );
+}
